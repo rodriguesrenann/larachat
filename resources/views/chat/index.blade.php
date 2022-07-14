fb64b430af52a45ac72c731422d94ad5a56756f9
Renan <renanrodrigues_cb_@hotmail.com>
2022-07-14 14:43:22 -0300

    Rota, view e controller para chat

---
+++ resources/views/chat/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', 'Chat')
+
+@section('content')
+    #chat
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/chat');
+
+Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
 
 Auth::routes();
 
