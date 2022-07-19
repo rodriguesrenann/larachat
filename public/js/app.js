0eaf89e147eccbc007a7dad65152aeb00cb5f508
Renan <renanrodrigues_cb_@hotmail.com>
2022-07-18 23:48:37 -0300

    Ajuste conf echo.js

--- public/js/app.js
+++ public/js/app.js
@@ -6107,7 +6107,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.io = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/lib/index.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'socket.io',
-  host: window.location.hostname + '6001'
+  host: window.location.hostname + ':6001'
 });
 
 __webpack_require__(/*! ./Echo */ "./resources/js/Echo.js");
--- routes/channels.php
+++ routes/channels.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-/* Broadcast::channel('chatroom', function ($user) {
+Broadcast::channel('chatroom', function ($user) {
     return $user;
-}) */;
+});
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
