2ddd5e857ecd6be19372052e36f114559e84b60c
Renan <renanrodrigues_cb_@hotmail.com>
2022-07-26 14:05:33 -0300

    Ordenação de usuários online

--- resources/js/vuex/modules/users/getters.js
+++ resources/js/vuex/modules/users/getters.js
@@ -1,3 +1,21 @@
 export default {
+    sortedUsers(state) {
+        let users = state.users.data
+        const onlineUsers = state.onlineUsers
 
+        //Ordenação dos usuários
+        users = users.sort(user => {
+            const index = onlineUsers.findIndex(u => u.email === user.email)
+
+            return index === -1 ? 1 : -1
+        })
+
+        //Usuários mapeados para online
+        users = users.map(user => {
+            const index = onlineUsers.findIndex(u => u.email === user.email)
+            user.online = index != -1
+        })
+
+        return users
+    }
 }
